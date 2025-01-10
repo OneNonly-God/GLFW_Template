8d3be471e8cb76a546af25833454ccb31268aa5c
OneNonly-God <basnetsarmila291@gmail.com>
2025-01-10 15:24:42 +0545

    Fixed bugs

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,14 @@ constexpr int WINDOW_WIDTH = 800;
 constexpr int WINDOW_HEIGHT = 600;
 constexpr char WINDOW_TITLE[] = "Game";
 
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+};
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
-}
+};
 
 int main() {
     if (!glfwInit()) {
@@ -61,7 +66,8 @@ int main() {
     Shader shader("res/shader/vertexShader.glsl", "res/shader/fragmentShader.glsl");
 
     while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+
+        processInput(window);
         glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -70,6 +76,7 @@ int main() {
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
     glDeleteVertexArrays(1, &VAO);
